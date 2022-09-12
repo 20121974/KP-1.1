21e164cbac4a611d847b93edc66e49875cd08390
Admin <nfi46102@mail.ru>
2022-09-12 10:13:37 +0300

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
-public class Easy {
-    private static Employee[] employees = new Employee[10];
-
+    public class Easy {
+        private static Employee[] employees = new Employee[10];
     }
+
     public static void main(String[] args) {
         Employee fio1 = new Employee("Иван", "Иванов", "Иванович", 1, 50000);
         Employee fio2 = new Employee("Петр", "Петров", "Петрович", 1, 50000);
         Employee fio3 = new Employee("Сидор", "Сидоров", "Сидорович", 2, 55000);
-        Employee fio4 = new Employee("Наумов", "Иванов", "Ильич", 2, 55000);
-        Employee fio5 = new Employee("Иван", "Иванов", "Иванович", 3, 60000);
-        Employee fio6 = new Employee("Иван", "Иванов", "Иванович", 3, 60000);
-        Employee fio7 = new Employee("Иван", "Иванов", "Иванович", 4, 65000);
-        Employee fio8 = new Employee("Иван", "Иванов", "Иванович", 4, 65000);
-        Employee fio9 = new Employee("Иван", "Иванов", "Иванович", 5, 70000);
-        Employee fio10 = new Employee("Иван", "Иванов", "Иванович", 5, 70000);
+        Employee fio4 = new Employee("Илья", "Наумов", "Ильич", 2, 55000);
+        Employee fio5 = new Employee("Петр", "Боев", "Иванович", 3, 60000);
+        Employee fio6 = new Employee("Игорь", "Мазилов", "Сергеевич", 3, 60000);
+        Employee fio7 = new Employee("Антон", "Ветров", "Олегович", 4, 65000);
+        Employee fio8 = new Employee("Олег", "Красулин", "Антонович", 4, 65000);
+        Employee fio9 = new Employee("Роман", "Морозов", "Иванович", 5, 70000);
+        Employee fio10 = new Employee("Денис", "Баев", "Иванович", 5, 70000);
 
         Easy.employees[0] = fio1;
         Easy.employees[1] = fio2;
@@ -25,7 +25,8 @@ public class Easy {
         Easy.employees[7] = fio8;
         Easy.employees[8] = fio9;
         Easy.employees[9] = fio10;
-}
+    }
+
     public static void printEmployees() {
         for (Employee employee : Easy.employees) {
             System.out.println(employee);
@@ -38,6 +39,40 @@ public class Easy {
             sum = sum + employee.getSalary();
         }
         return sum;
+    }
 
+    public static Employee findEmployeeWithMinSalary() {
+        Employee result = Easy.employees[0];
+        int minSalary = Easy.employees[0].getSalary();
+        for (Employee employee : Easy.employees) {
+            if (Easy.employees[0].getSalary() < minSalary) {
+                minSalary = employee.getSalary();
+                result = employee;
+            }
+        }
+        return result;
+    }
+
+    public static Employee findEmployeeWithMaxSalary() {
+        Employee result = Easy.employees[0];
+        int maxSalary = Easy.employees[0].getSalary();
+        for (Employee employee : Easy.employees) {
+            if (Easy.employees[0].getSalary() > maxSalary) {
+                maxSalary = employee.getSalary();
+                result = employee;
+            }
+        }
+        return result;
+    }
+
+    public static float calculateAverageSalary() {
+        float averageSalary = calculateTotalSalary() / (float) Easy.employees.length;
+        return averageSalary;
+    }
+
+    public static void printFullNames() {
+        for (Employee employee : Easy.employees) {
+            System.out.println(employee.getLastName());
+        }
     }
 }
